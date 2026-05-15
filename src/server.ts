0227a9862904aa929b9d0966d84df9fa27ea847d
alhaj-cmd <alhazsarkar5523@gmail.com>
2026-05-15 08:24:23 +0600

    express added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
-import express from "express"
-const app = express()
-const port = 3000
+import express, { type Application, type Request, type Response } from "express"
+const app : Application = express()
+const port = 5000;
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req : Request, res: Response) => {
+  // res.send('Hello World!')
+   res.status(200).json({"message" : "Express Server",
+    "author" : "Next Level",
+   })
+})
+
+app.post("/", async(req:Request, res: Response)=>{
+  // console.log(req.body);
+  const {name, email, pass} = req.body;
+  res.status(201).json({
+    message:"created",
+  data:{
+    name, email, pass
+  }
+  })
 })
 
 app.listen(port, () => {
