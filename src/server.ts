8f0dfa5d61cea487310d783df8fba6a5013167e4
alhaj-cmd <alhazsarkar5523@gmail.com>
2026-05-15 10:46:11 +0600

    add with server site postsql

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,39 @@
 import express, { type Application, type Request, type Response } from "express"
+import {Pool} from "pg";
 const app : Application = express()
 const port = 5000;
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({extended: true}));
+
+const pool = new Pool({
+  connectionString:"postgresql://neondb_owner:npg_cXshLbyaK9U6@ep-fragrant-king-aqxp6a6f-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
+const initDB = async()=>{
+  try{
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true, 
+      age INT,
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      
+      )
+      `)
+
+      console.log("Database conneted sucessfully")
+  }
+  catch(error){
+    console.log(error);
+  }
+};
+initDB();
 
 app.get('/', (req : Request, res: Response) => {
   // res.send('Hello World!')
